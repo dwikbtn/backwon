b51572614bbb01e4829b816a377f282ffc93559e
dwikbtn <40197095+dwikbtn@users.noreply.github.com>
2024-06-02 18:51:30 +0700

    add new route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
-const cors = require('cors');
-const midtransClient = require('midtrans-client');
+const cors = require("cors");
+const midtransClient = require("midtrans-client");
 
 const app = express();
 
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 // Read the database files
-const dbFile = fs.readFileSync('db.json');
-const cartFile = fs.readFileSync('cart.json');
+const dbFile = fs.readFileSync("db.json");
+const cartFile = fs.readFileSync("cart.json");
 
 // Parse JSON data
 const data = JSON.parse(dbFile);
@@ -20,8 +20,8 @@ const cartData = JSON.parse(cartFile);
 // Midtrans client configuration
 const snap = new midtransClient.Snap({
   isProduction: false, // Setel ke true jika Anda ingin menggunakan mode produksi
-  serverKey: 'SB-Mid-server-y7Jbk2UWIfvtDPYkNKCG0xlF', // Ganti dengan server key Anda
-  clientKey: 'SB-Mid-client-Fk9hLaOKsWxkcfTu' // Ganti dengan client key Anda
+  serverKey: "SB-Mid-server-y7Jbk2UWIfvtDPYkNKCG0xlF", // Ganti dengan server key Anda
+  clientKey: "SB-Mid-client-Fk9hLaOKsWxkcfTu", // Ganti dengan client key Anda
 });
 
 // Define routes
@@ -39,9 +39,9 @@ app.post("/cart", (req, res) => {
   }
 
   // Check if the product exists
-  const productExists = data.categories.some(category =>
-    category.products.some(product =>
-      product.id === newItem.id && product.type === newItem.type
+  const productExists = data.categories.some((category) =>
+    category.products.some(
+      (product) => product.id === newItem.id && product.type === newItem.type
     )
   );
 
@@ -51,14 +51,55 @@ app.post("/cart", (req, res) => {
 
   data.cart.push(newItem);
   // Save the updated cart data to the database file (db.json)
-  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+  fs.writeFileSync("db.json", JSON.stringify(data, null, 2));
   res.status(201).json(newItem); // Respond with the added item
+});
+
+app.checkout("/checkout", async (req, res) => {
+  const { orderId, amount, items, paymentInfo } = req.params;
+  const snap = new midtransClient.Snap({
+    isProduction: false, // Setel ke true jika Anda ingin menggunakan mode produksi
+    serverKey: "SB-Mid-server-y7Jbk2UWIfvtDPYkNKCG0xlF", // Ganti dengan server key Anda
+    clientKey: "SB-Mid-client-Fk9hLaOKsWxkcfTu", // Ganti dengan client key Anda
+  });
+
+  try {
+    const transactionDetails = {
+      transaction_details: {
+        order_id: orderId,
+        gross_amount: amount,
+      },
+      item_details: items.map((item) => ({
+        id: item.productId,
+        price: item.price,
+        quantity: item.quantity,
+        name: item.name,
+      })),
+      customer_details: {
+        email: paymentInfo.email,
+        first_name: paymentInfo.name,
+        ticket: paymentInfo.ticket,
+      },
+    };
+
+    const transaction = await snap.createTransaction(transactionDetails);
+    const responseData = {
+      redirect_url: transaction.redirect_url,
+    };
+
+    res.json(responseData);
+  } catch (error) {
+    console.error("Failed to create transaction:", error);
+    throw error;
+  }
 });
 
 // Get category by namepage
 app.get("/categories/:namepage", (req, res) => {
   const categoryNamePage = req.params.namepage;
-  const category = data.categories.find(cat => cat.namepage === categoryNamePage);
+  const category = data.categories.find(
+    (cat) => cat.namepage === categoryNamePage
+  );
   if (category) {
     res.json(category);
   } else {
@@ -69,7 +110,9 @@ app.get("/categories/:namepage", (req, res) => {
 // Get all products of a category by namepage
 app.get("/categories/:namepage/products", (req, res) => {
   const categoryNamePage = req.params.namepage;
-  const category = data.categories.find(cat => cat.namepage === categoryNamePage);
+  const category = data.categories.find(
+    (cat) => cat.namepage === categoryNamePage
+  );
   if (category) {
     res.json(category.products);
   } else {
@@ -81,9 +124,11 @@ app.get("/categories/:namepage/products", (req, res) => {
 app.get("/categories/:namepage/products/:productId", (req, res) => {
   const categoryNamePage = req.params.namepage;
   const productId = parseInt(req.params.productId);
-  const category = data.categories.find(cat => cat.namepage === categoryNamePage);
+  const category = data.categories.find(
+    (cat) => cat.namepage === categoryNamePage
+  );
   if (category) {
-    const product = category.products.find(prod => prod.id === productId);
+    const product = category.products.find((prod) => prod.id === productId);
     if (product) {
       res.json(product);
     } else {
@@ -97,7 +142,7 @@ app.get("/categories/:namepage/products/:productId", (req, res) => {
 // Get category by ID
 app.get("/categories/:id", (req, res) => {
   const categoryId = parseInt(req.params.id);
-  const category = data.categories.find(cat => cat.page === categoryId);
+  const category = data.categories.find((cat) => cat.page === categoryId);
   if (category) {
     res.json(category);
   } else {
@@ -108,7 +153,7 @@ app.get("/categories/:id", (req, res) => {
 // Get all products of a category
 app.get("/categories/:categoryId/products", (req, res) => {
   const categoryId = parseInt(req.params.categoryId);
-  const category = data.categories.find(cat => cat.page === categoryId);
+  const category = data.categories.find((cat) => cat.page === categoryId);
   if (category) {
     res.json(category.products);
   } else {
@@ -120,9 +165,9 @@ app.get("/categories/:categoryId/products", (req, res) => {
 app.get("/categories/:categoryId/products/:productId", (req, res) => {
   const categoryId = parseInt(req.params.categoryId);
   const productId = parseInt(req.params.productId);
-  const category = data.categories.find(cat => cat.page === categoryId);
+  const category = data.categories.find((cat) => cat.page === categoryId);
   if (category) {
-    const product = category.products.find(prod => prod.id === productId);
+    const product = category.products.find((prod) => prod.id === productId);
     if (product) {
       res.json(product);
     } else {
@@ -134,43 +179,43 @@ app.get("/categories/:categoryId/products/:productId", (req, res) => {
 });
 
 // Endpoint for creating transaction with Midtrans
-app.post('/create-transaction', async (req, res) => {
+app.post("/create-transaction", async (req, res) => {
   try {
     const { orderId, amount, items, paymentInfo } = req.body;
 
     // Debugging: Log received data
-    console.log('Received data:', { orderId, amount, items, paymentInfo });
+    console.log("Received data:", { orderId, amount, items, paymentInfo });
 
     const transactionDetails = {
       transaction_details: {
         order_id: orderId,
-        gross_amount: amount
+        gross_amount: amount,
       },
       item_details: items.map((item) => ({
         id: item.productId,
         price: item.price,
         quantity: item.quantity,
-        name: item.name
+        name: item.name,
       })),
       customer_details: {
         email: paymentInfo.email,
         first_name: paymentInfo.name,
-        ticket: paymentInfo.ticket
-      }
+        ticket: paymentInfo.ticket,
+      },
     };
 
     // Debugging: Log transaction details
-    console.log('Transaction details:', transactionDetails);
+    console.log("Transaction details:", transactionDetails);
 
     const transaction = await snap.createTransaction(transactionDetails);
 
     // Debugging: Log transaction result
-    console.log('Transaction result:', transaction);
+    console.log("Transaction result:", transaction);
 
     res.json({ redirect_url: transaction.redirect_url });
   } catch (error) {
-    console.error('Failed to create transaction:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Failed to create transaction:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
